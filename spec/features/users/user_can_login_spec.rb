2a9c03c09754090053a10072a096909c1bed2fb3
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-21 19:03:07 -0700

    Can not log in with someone elses email

--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'User log in' do
   
   before(:each) do
     @user  = create(:user)
-    @user2 = create(:user, name: 'Bilbo', email: 'bilbo@bilbo')
+    @user2 = create(:user, name: 'Bilbo', email: 'bilbo@bilbo', password: 'gandalf4lyfe')
   end
 
   it 'can login with proper credentials' do
@@ -38,6 +38,26 @@ RSpec.feature 'User log in' do
 
     within("form") do
       fill_in "email", with: "incorrect_email"
+      fill_in "password", with: @user.password
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Login failed')
+    expect(page).to have_link('Login')
+    expect(page).not_to have_link('Logout')
+  end
+
+  it "cannot log in from home page with email of another user" do
+    visit root_path
+
+    expect(page).to have_link("Login")
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+    
+    within("form") do
+      fill_in "email", with: @user2.email
       fill_in "password", with: @user.password
       click_on "Login"
     end
