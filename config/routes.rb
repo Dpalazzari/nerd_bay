e6bd8875d36a53011fec2dd034dde799250d3098
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-22 12:52:12 -0700

    Switch info from home page to about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, only: [:new, :create]
+  get '/about' => 'about#index'
   get '/dashboard' => 'users#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
