9dbd7561ffa844e0c272ded0e28ade0de3459173
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-21 18:27:40 -0700

    User create sad path

--- spec/features/visitor/visitor_can_create_an_account_spec.rb
+++ spec/features/visitor/visitor_can_create_an_account_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe "Visitor can create a new account" do
     expect(page).to_not have_link("Login")
     expect(page).to have_link("Logout")
   end
+
+  scenario 'account creation is unsuccessful' do
+    visit new_user_path
+
+    fill_in 'user[name]', with: "Bilbo"
+    fill_in 'user[email]', with: 'b@b'
+    click_on 'Create User'
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("All fields must be filled in to create account.")
+  end
 end
