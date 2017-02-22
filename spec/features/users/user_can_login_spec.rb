a80f9f4330777b69645e9f961af6c80907709b11
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-21 18:59:36 -0700

    Add fail to log in spec

--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -27,4 +27,24 @@ RSpec.feature 'User log in' do
     expect(page).to_not have_link("Login")
     expect(page).to have_link("Logout")
   end
+
+  it "cannot log in from home page with email that is not in user database" do
+    visit root_path
+
+    expect(page).to have_link("Login")
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+
+    within("form") do
+      fill_in "email", with: "incorrect_email"
+      fill_in "password", with: @user.password
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Login failed')
+    expect(page).to have_link('Login')
+    expect(page).not_to have_link('Logout')
+  end
 end
