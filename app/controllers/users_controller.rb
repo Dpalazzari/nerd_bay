f6b21aa675da600ce62bb28455996222248008f7
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-22 12:34:42 -0700

    Secure dashboard path for visitors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    if current_user
+      render :show
+    else
+      render file: "/public/404"
+    end
   end
 
   private
