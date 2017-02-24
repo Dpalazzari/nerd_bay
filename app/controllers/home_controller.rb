aaee716248d25f86b169394db9a6a684cc7f575f
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-24 09:32:53 -0700

    Remove commented out lines

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @presenter = HomePageAttributes.new
-    # @streams = Twitch.get_streams
-    # @movies  = MovieDatabase.now_playing 
   end
 
 end
