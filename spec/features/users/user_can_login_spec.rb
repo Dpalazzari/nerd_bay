bd8de47ec0e8c37b224cff95b54129f4317a9b26
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-23 21:26:14 -0700

    Refactor tests with vcr to speed up rspec

--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -8,63 +8,69 @@ RSpec.feature 'User log in' do
   end
 
   it 'can login with proper credentials' do
-    visit root_path
-    
-    expect(page).to have_link("Login")
-    click_on "Login"
-
-    expect(current_path).to eq(login_path)
-
-    within("form") do
-      fill_in "email", with: @user.email
-      fill_in "password", with: @user.password
+    VCR.use_cassette('logs_in_from_home_page') do
+      visit root_path
+      
+      expect(page).to have_link("Login")
       click_on "Login"
-    end
 
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Logged in as #{@user.name}")
-    expect(page).to have_content("Successfully logged in")
-    expect(page).to_not have_link("Login")
-    expect(page).to have_link("Logout")
+      expect(current_path).to eq(login_path)
+
+      within("form") do
+        fill_in "email", with: @user.email
+        fill_in "password", with: @user.password
+        click_on "Login"
+      end
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Logged in as #{@user.name}")
+      expect(page).to have_content("Successfully logged in")
+      expect(page).to_not have_link("Login")
+      expect(page).to have_link("Logout")
+    end
   end
 
   it "cannot log in from home page with email that is not in user database" do
-    visit root_path
+    VCR.use_cassette('fails_to_log_in') do
+      visit root_path
 
-    expect(page).to have_link("Login")
-    click_on "Login"
-
-    expect(current_path).to eq(login_path)
-
-    within("form") do
-      fill_in "email", with: "incorrect_email"
-      fill_in "password", with: @user.password
+      expect(page).to have_link("Login")
       click_on "Login"
-    end
 
-    expect(current_path).to eq(login_path)
-    expect(page).to have_content('Login failed')
-    expect(page).to have_link('Login')
-    expect(page).not_to have_link('Logout')
+      expect(current_path).to eq(login_path)
+
+      within("form") do
+        fill_in "email", with: "incorrect_email"
+        fill_in "password", with: @user.password
+        click_on "Login"
+      end
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content('Login failed')
+      expect(page).to have_link('Login')
+      expect(page).not_to have_link('Logout')
+    end
   end
 
   it "cannot log in from home page with email of another user" do
-    visit root_path
+    VCR.use_cassette('cannot_log_in_with_different_credentials') do
+      visit root_path
 
-    expect(page).to have_link("Login")
-    click_on "Login"
-
-    expect(current_path).to eq(login_path)
-    
-    within("form") do
-      fill_in "email", with: @user2.email
-      fill_in "password", with: @user.password
+      expect(page).to have_link("Login")
       click_on "Login"
-    end
 
-    expect(current_path).to eq(login_path)
-    expect(page).to have_content('Login failed')
-    expect(page).to have_link('Login')
-    expect(page).not_to have_link('Logout')
+      expect(current_path).to eq(login_path)
+      
+      within("form") do
+        fill_in "email", with: @user2.email
+        fill_in "password", with: @user.password
+        click_on "Login"
+      end
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content('Login failed')
+      expect(page).to have_link('Login')
+      expect(page).not_to have_link('Logout')
+    end
   end
 end
--- spec/features/users/user_can_logout_spec.rb
+++ spec/features/users/user_can_logout_spec.rb
@@ -8,22 +8,26 @@ RSpec.describe 'User can log out' do
 
   context 'a logged in user' do
     scenario 'a user should see a logout button' do
-      page.set_rack_session(user_id: @user.id)
-      visit root_path
+      VCR.use_cassette('user_sees_logout_button_on_home_page') do
+        page.set_rack_session(user_id: @user.id)
+        visit root_path
 
-      expect(page).to have_link('Logout')
-      expect(page).to have_content("Logged in as #{@user.name}")
+        expect(page).to have_link('Logout')
+        expect(page).to have_content("Logged in as #{@user.name}")
+      end
     end
 
     scenario 'a user clicks on logout' do
-      page.set_rack_session(user_id: @user.id)
-      visit root_path
+      VCR.use_cassette('user_clicks_on_logout_from_home_page') do
+        page.set_rack_session(user_id: @user.id)
+        visit root_path
 
-      click_on 'Logout'
-      expect(current_path).to eq(root_path)
-      expect(page).to have_link('Login')
-      expect(page).to_not have_link('Logout')
-      expect(page).to have_content("See you later, nerd.")
+        click_on 'Logout'
+        expect(current_path).to eq(root_path)
+        expect(page).to have_link('Login')
+        expect(page).to_not have_link('Logout')
+        expect(page).to have_content("See you later, nerd.")
+      end
     end
   end
 
