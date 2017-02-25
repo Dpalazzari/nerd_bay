0bfdc83c9b23e78d564f0391a6e8c75fd5443993
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-25 13:13:39 -0700

    Style changes
    Fixed the styles for the articles on the home page

--- app/site_models/hacker_articles.rb
+++ app/site_models/hacker_articles.rb
@@ -1,5 +1,12 @@
 class HackerArticles
 
+  attr_reader :title, :url
+
+  def initialize(attributes={})
+    @title = attributes[:title]
+    @url   = attributes[:url]
+  end
+
   def self.top_articles
     HackerService.get_articles.map do |article|
       new(article)
