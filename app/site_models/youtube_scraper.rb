668471a15a6cd0d9d5cfc8e55795981123244440
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-01 18:19:37 -0700

    fix youtube scraper

--- app/site_models/youtube_scraper.rb
+++ app/site_models/youtube_scraper.rb
@@ -1,20 +1,13 @@
 class YoutubeScraper
 
   def self.get_most_popular
-    node     = html.css(".yt-lockup-thumbnail").first
-    top_link = Nokogiri::HTML(node.inner_html).css('a').first['href']
-    link     = top_link.chomp[9..-1]
-    link
-  end
-
-  def self.http
-    Curl.get('https://www.youtube.com/feed/trending') do |http|
+    http = Curl.get('https://www.youtube.com/feed/trending') do |http|
       http.headers['User-Agent'] = "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/21.0"
     end
-  end
 
-  def self.html
-    Nokogiri::HTML(http.body_str)
+    html = Nokogiri::HTML(http.body_str)
+    node = html.css(".yt-lockup-thumbnail").first
+    top_link = Nokogiri::HTML(node.inner_html).css('a').first['href']
+    link = top_link.chomp[9..-1]
   end
-
 end
